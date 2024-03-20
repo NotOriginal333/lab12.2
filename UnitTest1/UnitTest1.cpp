ff49624fa9ae231794cda3266027404db7ef150b
NotOriginal333 <m.naumovych2@gmail.com>
2024-03-20 20:48:16 +0200

    Add project files.

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,50 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab12.2/lab12.2it.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+
+{
+    bool compareLists(Node* list1, Node* list2) {
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->data != list2->data) {
+                return false;
+            }
+            list1 = list1->next;
+            list2 = list2->next;
+        }
+        return (list1 == nullptr && list2 == nullptr);
+    }
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+            Node* L = nullptr;
+            append(L, 1);
+            append(L, -2);
+            append(L, 3);
+            append(L, -4);
+            append(L, 5);
+
+            Node* L1_expected = nullptr;
+            append(L1_expected, 1);
+            append(L1_expected, 3);
+            append(L1_expected, 5);
+
+            Node* L2_expected = nullptr;
+            append(L2_expected, -2);
+            append(L2_expected, -4);
+
+            Node* L1_actual = nullptr;
+            Node* L2_actual = nullptr;
+            splitList(L, L1_actual, L2_actual);
+
+            Assert::IsTrue(compareLists(L1_actual, L1_expected));
+
+            Assert::IsTrue(compareLists(L2_actual, L2_expected));
+		}
+	};
+}
---
+++ lab12.2/lab12.2it.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int value) : data(value), next(nullptr) {}
+};
+
+// Додавання елемента в кінець списку
+void append(Node*& head, int value) {
+    Node* newNode = new Node(value);
+    if (head == nullptr) {
+        head = newNode;
+        return;
+    }
+    Node* temp = head;
+    while (temp->next != nullptr) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+// Виведення значень елементів списку на екран
+void printList(Node* head) {
+    while (head != nullptr) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+// Формування списків L1 та L2 з додатних та від'ємних значень відповідно
+void splitList(Node* head, Node*& positiveList, Node*& negativeList) {
+    Node* temp = head;
+    while (temp != nullptr) {
+        if (temp->data > 0) {
+            append(positiveList, temp->data);
+        }
+        else if (temp->data < 0) {
+            append(negativeList, temp->data);
+        }
+        temp = temp->next;
+    }
+}
+
+int main() {
+    // Формування початкового списку
+    Node* L = nullptr;
+    append(L, 1);
+    append(L, -2);
+    append(L, 3);
+    append(L, -4);
+    append(L, 5);
+
+    // Виведення початкового списку на екран
+    cout << "Initial List: ";
+    printList(L);
+
+    // Формування списків L1 та L2
+    Node* L1 = nullptr; // Список для додатних значень
+    Node* L2 = nullptr; // Список для від'ємних значень
+    splitList(L, L1, L2);
+
+    // Виведення списків L1 та L2 на екран
+    cout << "List with positive values (L1): ";
+    printList(L1);
+    cout << "List with negative values (L2): ";
+    printList(L2);
+
+    return 0;
+}
